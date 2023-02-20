2ddd972f88e15feb1387a42f531c904f41fe76c4
LILPUMPSDAD <92663856+LILPUMPSDAD@users.noreply.github.com>
2023-02-19 18:26:03 -0800

    ORL Fixed

--- code/stage_3_code/Method_CNN.py
+++ code/stage_3_code/Method_CNN.py
@@ -14,10 +14,11 @@ from code.stage_3_code.Evaluate_Accuracy import Evaluate_Accuracy
 
 class Method_CNN(method, nn.Module):
     data = None
-    max_epoch = 1
+    max_epoch = 10
     learning_rate = 1e-3
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     def __init__(self, mName, mDescription, dataset):
         method.__init__(self, mName, mDescription)
         nn.Module.__init__(self)
@@ -26,72 +27,110 @@ class Method_CNN(method, nn.Module):
 
         if self.dataset == 'MNIST':
             self.conv1 = nn.Sequential(
-                nn.Conv2d(
-                    in_channels=1,
-                    out_channels=16,
-                    kernel_size=5,
-                    stride=1,
-                    padding=2,
-                ),
+                nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=2, ),
                 nn.ReLU(),
                 nn.MaxPool2d(kernel_size=2),
             ).to(self.device)
+
             self.conv2 = nn.Sequential(
                 nn.Conv2d(16, 32, 5, 1, 2),
                 nn.ReLU(),
                 nn.MaxPool2d(2),
             ).to(self.device)
+
             self.out = nn.Linear(32 * 7 * 7, 10).to(self.device)
+
         elif self.dataset == 'ORL':
-            self.conv_layer_1 = nn.Conv2d(3, 32, 3, 1).to(self.device)
-            self.activation_func_1 = nn.ReLU().to(self.device)
-            self.conv_layer_2 = nn.Conv2d(32, 32, 3, 1).to(self.device)
-            self.conv_layer_3 = nn.Conv2d(32, 64, 3, 1).to(self.device)
-            self.fc_layer_1 = nn.Linear(3 * 2 * 64, 128).to(self.device)
-            self.fc_layer_2 = nn.Linear(128, 40).to(self.device)
-            self.activation_func_2 = nn.LogSoftmax(dim=1).to(self.device)
+            self.Normal_ORL(3, 40, 28, 23)
+
         elif self.dataset == 'CIFAR':
-            self.conv1 = nn.Sequential(
-                nn.Conv2d(
-                    in_channels=3,
-                    out_channels=16,
-                    kernel_size=5,
-                    stride=1,
-                    padding=2,
-                ),
-                nn.ReLU(),
-                nn.MaxPool2d(kernel_size=2),
-            ).to(self.device)
-            self.conv2 = nn.Sequential(
-                nn.Conv2d(16, 32, 5, 1, 2),
-                nn.ReLU(),
-                nn.MaxPool2d(2),
-            ).to(self.device)
-            self.out = nn.Linear(32 * 8 * 8, 10).to(self.device)
+            self.Normal_CIFAR()
+
+    def Normal_CIFAR(self):
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        ).to(self.device)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        ).to(self.device)
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        ).to(self.device)
+        self.fc1 = nn.Sequential(
+            nn.Linear(256 * 4 * 4, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+        ).to(self.device)
+        self.fc2 = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+        ).to(self.device)
+        self.fc3 = nn.Linear(512, 10).to(self.device)
+
+    def Normal_CIFARf(self, x):
+        x = self.conv1(x).to(self.device)
+        x = self.conv2(x).to(self.device)
+        x = self.conv3(x).to(self.device)
+        x = x.view(x.size(0), -1).to(self.device)
+        x = self.fc1(x).to(self.device)
+        x = self.fc2(x).to(self.device)
+        output = self.fc3(x).to(self.device)
+        return output
+
+    def Normal_ORL(self, inchannels=3, classes=10, x=8, y=8):
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=inchannels, out_channels=16, kernel_size=5, stride=1, padding=2, ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        ).to(self.device)
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        ).to(self.device)
+
+        self.out = nn.Linear(32 * x * y, classes).to(self.device)
+
+    def Normal_ORLF(self, x):
+        x = self.conv1(x).to(self.device)
+        x = self.conv2(x).to(self.device)
+        x = x.view(x.size(0), -1).to(self.device)
+        output = self.out(x).to(self.device)
+        return output
 
     def forward(self, x):
         if self.dataset == 'MNIST':
-            x = self.conv1(x)
-            x = self.conv2(x)  # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+            x = self.conv1(x).to(self.device)
+            x = self.conv2(x).to(self.device)
             x = x.view(x.size(0), -1).to(self.device)
-            output = self.out(x)
+            output = self.out(x).to(self.device)
             return output
         elif self.dataset == 'ORL':
-            h = self.activation_func_1(self.conv_layer_1(x))
-            h = nn.MaxPool2d(5).to(self.device)(h)
-            h = nn.ReLU().to(self.device)(self.conv_layer_2(h))
-            h = nn.MaxPool2d(4).to(self.device)(h)
-            h = nn.ReLU().to(self.device)(self.conv_layer_3(h))
-            h = torch.flatten(h, 1)
-            h = nn.ReLU().to(self.device)(self.fc_layer_1(h))
-            y_pred = self.activation_func_2(self.fc_layer_2(h))
-            return y_pred
+            return self.Normal_ORLF(x)
         elif self.dataset == 'CIFAR':
-            x = self.conv1(x)
-            x = self.conv2(x)  # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
-            x = x.view(x.size(0), -1).to(self.device)
-            output = self.out(x)
-            return output
+            return self.Normal_CIFARf(x)
 
     def train(self, data):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
@@ -108,9 +147,9 @@ class Method_CNN(method, nn.Module):
                 train_loss.backward()
                 optimizer.step()
 
-            if epoch%1 == 0:
+            if epoch % 1 == 0:
                 accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
-                print('Epoch:', epoch,
+                print('Epoch:', epoch + 1,
                       'Accuracy:', accuracy_evaluator.evaluate_accuracy(),
                       'F1', accuracy_evaluator.evaluate_F1(),
                       'Precision', accuracy_evaluator.evaluate_precision(),
@@ -132,7 +171,7 @@ class Method_CNN(method, nn.Module):
         y_pred_list = torch.flatten(y_pred_list)
 
         return y_pred_list, y_true_list
-    
+
     def run(self):
         print('method running...')
         print('--start training...')
