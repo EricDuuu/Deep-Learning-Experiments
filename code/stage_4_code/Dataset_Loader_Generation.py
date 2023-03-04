983d4872102d6e88e80cf623d6c468081508317a
bhullaragam <agambhullar313@gmail.com>
2023-03-04 12:36:33 -0800

    data_generation

--- code/stage_4_code/Dataset_Loader_Generation.py
+++ code/stage_4_code/Dataset_Loader_Generation.py
@@ -6,6 +6,15 @@ Concrete IO class for a specific dataset
 # License: TBD
 
 from code.base_class.dataset import dataset
+import pickle
+import os
+import re
+import string
+import torch
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from os.path import exists
+import numpy as np
 
 
 class Dataset_Loader(dataset):
@@ -16,32 +25,78 @@ class Dataset_Loader(dataset):
     train_data = None
     train_path = None
     train_file_name = None
-    
+
     def __init__(self, dName=None, dDescription=None):
         super().__init__(dName, dDescription)
-    
+
+
+    def preprocess_text(self, text):
+        text = text.lower()
+        text_numfree = re.sub(r'\d+', '', text)
+        text_punctfree = text_numfree.translate(str.maketrans('', '', string.punctuation))
+
+        # coming back to removing stop words
+        text_embd_tokens = word_tokenize(text_punctfree)
+        tok_clean_sw = [word for word in text_embd_tokens if not word in stopwords.words()]
+        text_tok_clean = (" ").join(tok_clean_sw)
+
+        text_stripped = re.sub('\s+', ' ', text_tok_clean).strip()
+
+        return text_stripped
+
+    def data_split(self, test_size=0.2):
+
+
+        #x_t = torch.Tensor()
+        #y_t = torch.Tensor()
+
+        #vals = x.shape[0]
+
+        with open('', r) as file:
+            text = file.readlines()
+
+        x = []
+        y = []
+        for line in text:
+            proc_text = self.preprocess(line)
+            vals = proc_text.split()
+            x.append(vals[:-1])
+            y.append(vals[:-1])
+
+        sp_point = int(x[0] * (1 - test_size))
+
+        x_t = torch.Tensor(x)
+        y_t = torch.Tensor(y)
+
+
+
+
+
+        x_train = x_t[:sp_point]
+        y_train = y_t[:sp_point]
+        x_test = x_t[sp_point:]
+        y_test = y_t[sp_point:]
+
+        return x_train, y_train, x_test, y_test
+
+
+
+
+
     def load(self):
         print('loading data...')
+        text_gen_path = '../../data/stage_4_data/text_generation/data'
         X_test = []
         y_test = []
 
         X_train = []
         y_train = []
 
-        f = open(self.test_path + self.test_file_name, 'r')
-        for line in f:
-            line = line.strip('\n')
-            elements = [int(i) for i in line.split(',')]
-            X_test.append(elements[1:])
-            y_test.append(elements[0])
-        f.close()
+        for filename in os.listdir(text_gen_path + "/data"):
+            with open(os.path.join(text_gen_path + "/data", filename), 'r', encoding='utf8') as file:
+                text = file.read()
+                X_test.append(self.preprocess_text(text))
 
-        f = open(self.train_path + self.train_file_name, 'r')
-        for line in f:
-            line = line.strip('\n')
-            elements = [int(i) for i in line.split(',')]
-            X_train.append(elements[1:])
-            y_train.append(elements[0])
-        f.close()
+        #X_train, y_train, X_test, y_test = self.data_split()
 
         return {'train': {'X': X_train, 'y': y_train}, 'test': {'X': X_test, 'y': y_test}}
