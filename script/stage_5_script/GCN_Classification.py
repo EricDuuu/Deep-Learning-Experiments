1226835c18379aa944d3d617e6077cf15b13b9de
LILPUMPSDAD <92663856+LILPUMPSDAD@users.noreply.github.com>
2023-03-13 23:40:46 -0700

    done stage 5

--- script/stage_5_script/GCN_Classification.py
+++ script/stage_5_script/GCN_Classification.py
@@ -3,6 +3,7 @@ import torch.optim as optim
 
 from code.stage_5_code.Dataset_Loader_Node_Classification import Dataset_Loader
 from code.stage_5_code.GCN_Model_Classification import GCN, train, evaluate, test
+from code.stage_5_code.Plotter import plot_acc_loss
 
 # Load the dataset
 dataset_name = "citeseer"
@@ -21,7 +22,7 @@ labels = data['graph']['y']
 
 # Initialize the model, optimizer, and loss function
 epochs = 500
-dropout = 0.3 # Best : 0.5
+dropout = 0.5 # Best : 0.5
 weight_decay = 5e-4 # Best: 5e-4
 hidden_dim = 128
 model = GCN(input_features=input_features.shape[1], hidden_dim=hidden_dim,
@@ -35,13 +36,20 @@ patience = 100
 counter = 0
 best_val_acc = 0
 best_model = None
+val_accs = []
+val_losses = []
 
 # Train the model with early stopping
 for epoch in range(epochs):
     loss, train_eval = train(model, optimizer, criterion, input_features, adj_list, labels, train_idx)
     val_loss, val_eval = evaluate(model, criterion, input_features, adj_list, labels, val_idx)
     f1, accuracy, recall, precision = val_eval
-    print('Epoch {}/{}: Train Loss {:.4f} | Val Loss {:.4f} | Val Acc {:.4f}'.format(epoch + 1, epochs, loss, val_loss, accuracy))
+    val_losses.append(val_loss)
+    val_accs.append(accuracy)
+    print(
+        'Epoch {}/{}: Train Loss {:.4f} | Val Loss {:.4f} | Val Acc {:.4f} '
+        '| Val F1 {:.4f} | Val Recall {:.4f} | Val Precision {:.4f}'.format(
+            epoch + 1, epochs, loss, val_loss, accuracy, f1, recall, precision))
 
     # Check if the model has improved on the validation set
     if accuracy > best_val_acc:
@@ -60,4 +68,8 @@ for epoch in range(epochs):
 model.load_state_dict(best_model)
 test_loss, test_eval = test(model, criterion, input_features, adj_list, labels, test_idx)
 f1, accuracy, recall, precision = test_eval
-print('Test Loss: {:.4f}, Test Accuracy: {:.4f}'.format(test_loss, accuracy))
+print('Test Loss {:.4f} | Test Acc {:.4f} | Test F1 {:.4f} |'
+      ' Test Recall {:.4f} | Test Precision {:.4f}'.format(
+            test_loss, accuracy, f1, recall, precision))
+
+plot_acc_loss("GCN", val_accs, val_loss)
